83364ddc18919bd05c0acb921bc4dc7a01942582
AndreBaixista1 <andrbter@gmail.com>
2022-05-30 11:50:28 -0300

    Novos louvores

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 var items = [
   {
-    name: "A casa é sua",
-    author: "Casa Worship",
-    type: "Convocação",
-  },
-  {
     name: "A Ele a Glória",
     author: "Diante do trono",
     type: "Declaração",
-  },
-  {
-    name: "A igreja vem",
-    author: "Anderson Freire",
-    type: "Convocação",
   },
   {
     name: "A tua glória me atrai",
@@ -23,6 +13,17 @@ var items = [
     name: "Abba",
     author: "Laura Souguelis",
     type: "Encontro",
+  },
+  //    ---- Convocação ----
+  {
+    name: "A casa é sua",
+    author: "Casa Worship",
+    type: "Convocação",
+  },
+  {
+    name: "A igreja vem",
+    author: "Anderson Freire",
+    type: "Convocação",
   },
   {
     name: "Abra os olhos do meu coração",
@@ -60,54 +61,100 @@ var items = [
     type: "Convocação",
   },
   {
-      name:"Cultura do céu",
-      author: "Davi Fernandes",
-      type:"Convocação"
+    name: "Cultura do céu",
+    author: "Davi Fernandes",
+    type: "Convocação",
   },
   {
-      name: "Deus está aqui",
-      author:"Eli Soares",
-      type:"Convocação"
+    name: "Deus está aqui",
+    author: "Eli Soares",
+    type: "Convocação",
   },
   {
-      name: "Eu vejo a glória",
-      author:"Fernanda Brum",
-      type:"Convocação"
+    name: "Eu vejo a glória",
+    author: "Fernanda Brum",
+    type: "Convocação",
   },
   {
-      name:"Festa",
-      author:"Davi Sacer",
-      type: "Convocação"
+    name: "Festa",
+    author: "Davi Sacer",
+    type: "Convocação",
   },
   {
-      name: "Hosana",
-      author:"Gabriela Rocha",
-      type:"Convocação"
+    name: "Hosana",
+    author: "Gabriela Rocha",
+    type: "Convocação",
   },
   {
-      name:"Liberdade",
-      author:"Laura Souguelis"
+    name: "Liberdade",
+    author: "Laura Souguelis",
+    type: "Convocação",
   },
   {
-      name:"Mil graus",
-      author:"Renascer Praise",
-      type:"Convocação"
+    name: "Mil graus",
+    author: "Renascer Praise",
+    type: "Convocação",
   },
   {
-      name:"Para que entre o rei da glória",
-      author:"Morada",
-      type:"Convocação"
+    name: "Para que entre o rei da glória",
+    author: "Morada",
+    type: "Convocação",
   },
   {
-      name:"Prosperarei",
-      author:"Trazendo a arca",
-      type:"Convocação"
+    name: "Prosperarei",
+    author: "Trazendo a arca",
+    type: "Convocação",
   },
   {
-      name:"Quando a igreja louva",
-      author:"Davi Sacer",
-      type:"Convocação"
-  }
+    name: "Quando a igreja louva",
+    author: "Davi Sacer",
+    type: "Convocação",
+  },
+  {
+    name: "Que se abram os céus",
+    author: "Nivea Soares",
+    type: "Convocação",
+  },
+  {
+    name: "Rei meu",
+    author: "Aline Barros",
+    type: "Convocação",
+  },
+  {
+    name: "Santo Espirito",
+    author: "Laura Souguelis",
+    type: "Convocação",
+  },
+  {
+    name: "Santo, Santo, Santo",
+    author: "Renascer Praise",
+    type: "Convocação",
+  },
+  {
+    name: "Senhor te quero (eu te busco)",
+    author: "Vineyard",
+    type: "Convocação",
+  },
+  {
+    name: "Vem Senhor enche este lugar",
+    author: "Quatro por um",
+    type: "Convocação",
+  },
+  {
+    name: "Vitória no deserto",
+    author: "Aline Barros",
+    type: "Convocação",
+  },
+  {
+    name: "Voar como águia",
+    author: "Renascer Praise",
+    type: "Convocação",
+  },
+  {
+    name: "Vou te alegrar",
+    author: "Aline Barros",
+    type: "Convocação",
+  },
 ];
 
 var btnEscolha = document.getElementById("show_music");
@@ -118,16 +165,19 @@ btnEscolha.addEventListener("click", () => {
   spanWindow.innerHTML = "";
   var listaItens = [];
   var qtdeMusic = document.getElementById("quantity").value;
-  for (var i = 0; i <= qtdeMusic - 1; i++) {
-    var r = Math.floor(Math.random() * items.length);
-    // console.log(items[r].name);
-    if (listaItens.indexOf(items[r].name) == -1) {
-      listaItens.push(items[r].name);
-      
-      var li = document.createElement("li");
+  if (qtdeMusic <= 0) {
+    alert("Digite um valor válido !");
+  } else {
+    for (var i = 0; i <= qtdeMusic - 1; i++) {
+      var r = Math.floor(Math.random() * items.length);
+      // console.log(items[r].name);
+      if (listaItens.indexOf(items[r].name) == -1) {
+        listaItens.push(items[r].name);
+
+        var li = document.createElement("li");
         li.innerHTML = `${items[r].name} - ${items[r].author} - ${items[r].type}`;
         spanWindow.appendChild(li);
-      
+      }
     }
   }
 });
